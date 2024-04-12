83e16e935e3c54e9f8cff99b83971436cf9e4ba5
tal.shaham <talso12345@gmail.com>
2024-04-12 16:43:17 +0300

    lalala

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "hello world";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
